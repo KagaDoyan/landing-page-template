7cdf7fb27c49178d8fb08dcf5b1fb0cf7fadd21d
jack <phanoudeth@kokkok.la>
2024-11-11 15:26:45 +0700

    new init

--- src/components/layouts/quickcontact.tsx
+++ src/components/layouts/quickcontact.tsx
@@ -1,5 +1,5 @@
 // components/QuickContact.tsx
-
+'use client';
 import React from 'react';
 import { Box, IconButton } from '@mui/material';
 import ChatIcon from '@mui/icons-material/Chat';
@@ -30,6 +30,10 @@ const QuickContact: React.FC = () => {
               transform: 'scale(1.2)', // Enlarge the icon inside
             },
           },
+        }}
+        onClick={() => {
+          // open line new tab
+          window.open('https://line.me/ti/p/%2B6612345678', '_blank');
         }}
       >
         <FaLine fontSize="40px" color="green" />
